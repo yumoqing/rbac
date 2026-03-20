8ed0cb9c494efdd6659adb56a707aba757907163
yumoqing <yumoqing@gmail.com>
2026-03-20 18:30:31 +0800

    bugfix

--- rbac/userperm.py
+++ rbac/userperm.py
@@ -44,6 +44,8 @@ where a.id = c.userid
 	def check_roles_path(self, roles, path):
 		for role in roles:
 			paths = self.rp_caches.get(role)
+			if not paths:
+				return False
 			return path in paths
 
 	async def is_user_has_path_perm(self, userid, path):
